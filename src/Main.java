e73509a7e1d751214c1b2cb13c50ef215c4c3b2b
Tiago Silva <tiago.asilva97@gmail.com>
2024-11-21 16:01:07 +0000

    Removed unnecessary imports from main class

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,9 @@
-import com.tiago.algorithms.array.ArrayExercises;
-import com.tiago.algorithms.manager.ExerciseManager;
-import com.tiago.algorithms.math.MathExercises;
-import com.tiago.algorithms.string.StringExercises;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.tiago.algorithms.manager.ExerciseManager;
+
 public class Main {
     public static void main(String[] args) {
-
         ExerciseManager exerciseManager = new ExerciseManager();
-
     }
 
 }
